9c5c9e4632632dc6e274ef56e4a8a49d32d29e27
Ashley Kumar <askumar@andrew.cmu.edu>
2013-04-14 03:55:18 -0400

    first commit

---
+++ polynomial.h
@@ -0,0 +1,17 @@
+// Ashley Kumar
+// 2013
+
+
+// Fraction struct to represent rational coefficients 
+struct Rational {
+  int num;
+  int den; 
+}; 
+
+
+
+struct Polynomial {
+  Rational** coeff; 
+};
+
+
